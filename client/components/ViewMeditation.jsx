40259339d5658243a88d9458e58ddf12f2cd19ae
alex-bartleynees <73970031+alex-bartleynees@users.noreply.github.com>
2021-04-17 16:22:14 +1200

    Try to get total time working

--- client/components/ViewMeditation.jsx
+++ client/components/ViewMeditation.jsx
@@ -7,32 +7,49 @@ const ViewMeditation = (props) => {
   const id = props.match.params.id
 
   const [meditation, setMeditation] = useState([])
-
+  
   useEffect(() => {
     getAllMeditations()
       .then((res) => {
         setMeditation(res.filter(ameditation => ameditation.user_id == id))
       })
   }, [])
-  console.log(meditation)
+
+
+ 
+  // const time = meditation.map(ameditation => ameditation.time)
+
+  // function totalTime(arr) {
+  
+  //   const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  //   return arr.reduce(reducer) / 60;
+    
+
+  // }
+  // const finalTime = totalTime(time)
+  // console.log(finalTime)
+ 
+
   return (
 
     <div className="column">
       <div className="columns is-centered m-5">
         <ul>
-        {meditation.map(ameditation => {
-          return <li key={ameditation.id} className= "has-text-centered m-4"> <h2 className="title is-5">
-           {ameditation.meditation_name} 
-           </h2>
-          <p className="subtitle"> Time: {ameditation.time} </p>
-          <p className="subtitle">Date: {ameditation.date} </p>
-          
-           </li>
-  
-        })}
-        </ul>
-      </div>
+          {meditation.map(ameditation => {
+            return <li key={ameditation.id} className="has-text-centered m-4"> <h2 className="title is-5">
+              {ameditation.meditation_name}
+            </h2>
+              <p className="subtitle"> Time: {ameditation.time} </p>
+              <p className="subtitle">Date: {ameditation.date} </p>
 
+            </li>
+
+          })}
+        </ul>
+        
+      </div>
+          {/* Total Time: {totalTime(finalTime)} */}
     </div>
   )
 }
